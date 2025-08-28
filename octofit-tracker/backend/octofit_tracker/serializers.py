20e46b04ae1763bbeb8b708a758ec390c3df98bd
John Vanderpool <vpool88@gmail.com>
2025-08-28 21:16:23 +0000

    fix: set REACT_APP_API_URL in .env for Codespace backend connectivity

--- octofit-tracker/backend/octofit_tracker/serializers.py
+++ octofit-tracker/backend/octofit_tracker/serializers.py
@@ -1,27 +1,39 @@
 from rest_framework import serializers
 from .models import User, Team, Activity, Workout, Leaderboard
 
+
 class UserSerializer(serializers.ModelSerializer):
+    id = serializers.CharField(source='_id', read_only=True)
     class Meta:
         model = User
-        fields = '__all__'
+        fields = ['id', 'email', 'name', 'team', 'is_superhero']
+
 
 class TeamSerializer(serializers.ModelSerializer):
+    id = serializers.CharField(source='_id', read_only=True)
     class Meta:
         model = Team
-        fields = '__all__'
+        fields = ['id', 'name', 'description']
+
 
 class ActivitySerializer(serializers.ModelSerializer):
+    id = serializers.CharField(source='_id', read_only=True)
+    user = serializers.CharField(source='user.id', read_only=True)
     class Meta:
         model = Activity
-        fields = '__all__'
+        fields = ['id', 'user', 'type', 'duration', 'date']
+
 
 class WorkoutSerializer(serializers.ModelSerializer):
+    id = serializers.CharField(source='_id', read_only=True)
     class Meta:
         model = Workout
-        fields = '__all__'
+        fields = ['id', 'name', 'description', 'suggested_for']
+
 
 class LeaderboardSerializer(serializers.ModelSerializer):
+    id = serializers.CharField(source='_id', read_only=True)
+    team = serializers.CharField(source='team.name', read_only=True)
     class Meta:
         model = Leaderboard
-        fields = '__all__'
+        fields = ['id', 'team', 'points']
